977b796abd5c86058224d6a9e556c5bf74e49867
Michal wolf <wolfm23@spstrutnov.cz>
2024-10-04 08:08:01 +0200

    základ

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace zaklady_her
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
