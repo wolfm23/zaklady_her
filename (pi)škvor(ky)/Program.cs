a266652595f0bfe1392aef69cc7ef1b98be81a86
Michal wolf <wolfm23@spstrutnov.cz>
2024-10-14 12:15:21 +0200

    (pi)škvor(ky) opravdu založeny

---
+++ (pi)škvor(ky)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _pi_škvor_ky_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
